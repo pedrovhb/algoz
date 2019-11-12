54ad27ec2b5f0ccb62b1cb202ba500d3cc5b0dac
Pedro von Hertwig <pedrovhb@gmail.com>
2019-11-11 21:38:36 -0300

    Implements Min K heap with memory-efficient O(2^k) space complexity and O(logK) insertion, O(1) peek

--- structures/min_k_heap.py
+++ structures/min_k_heap.py
@@ -11,7 +11,7 @@ class MinKHeap:
         self._array.append(val)
 
         index = len(self._array) - 1
-        while True:
+        while index > 0:
             parent_index = (index - 1) // 2
             if self._array[parent_index] > val:
                 self._array[parent_index], self._array[index] = self._array[index], self._array[parent_index]
@@ -19,20 +19,20 @@ class MinKHeap:
             else:
                 break
 
-        if len(self._array) > 2 ** (self.max_k + 1):
-            self._array = self._array[:2 ** (self.max_k + 1)]
+        if len(self._array) > 2 ** self.max_k:
+            self._array = self._array[:2 ** self.max_k]
 
     def low(self):
         return self._array[0]
 
 
-l = [random.randint(0, 1000) for _ in range(3000)]
+l = [random.randint(0, 10000) for _ in range(3000)]
 
 mkh = MinKHeap(5)
 for num in l:
     mkh.add(num)
 
-print(sorted(mkh._array)[:5])
-print(sorted(l)[:5])
-
-print(len(l), len(mkh._array))
+print('Min 5 MinKHeap:', sorted(mkh._array)[:5])
+print('Min 5 list:', sorted(l)[:5])
+print('Length of MinKHeap', len(mkh._array))
+print('Length of list:', len(l))
